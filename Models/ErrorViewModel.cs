3ddb55290699c7840cf6246ae12bc551bc8c3f9f
TXT635 <atruong0306@gmail.com>
2023-04-24 03:47:34 -0700

    Addpackage

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TranXuanTruong0635.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
