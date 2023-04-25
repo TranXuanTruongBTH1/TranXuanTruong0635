dbd953f561519ea9c4ebace9abf9f012ad561ff9
TXT635 <atruong0306@gmail.com>
2023-04-24 23:33:32 -0700

    addPackage

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TranXuanTruong0635.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
